e37d537274646a86ea29f8610e3703cf26af6d6c
kameleon07 <37112322+kameleon07@users.noreply.github.com>
2018-10-17 10:36:22 +0100

    Update

--- PinkPineapleDating2/app/src/main/java/com/pinkpineaple/pinkpineapledating/MainActivity.java
+++ PinkPineapleDating2/app/src/main/java/com/pinkpineaple/pinkpineapledating/MainActivity.java
@@ -1,105 +1,59 @@
 package com.pinkpineaple.pinkpineapledating;
 
-import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
-import android.content.Intent;
-
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
 import android.widget.ArrayAdapter;
-import android.app.AlertDialog;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView lvDiscussionTopics;
-    ArrayList<String> listOfDiscussion = new ArrayList<String>();
-    ArrayAdapter arrayAdpt;
-
-    String UserName;
-
-
-    private DatabaseReference dbr = FirebaseDatabase.getInstance().getReference().getRoot();
+    private ListView listView;
+    private View btnSend;
+    private EditText editText;
+    boolean myMessage = true;
+    private List<ChatBubble> ChatBubbles;
+    private ArrayAdapter<ChatBubble> adapter;
 
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ChatBubbles = new ArrayList<>();
 
+        listView = (ListView) findViewById(R.id.list_msg);
+        btnSend = findViewById(R.id.btn_chat_send);
+        editText = (EditText) findViewById(R.id.msg_type);
 
-        lvDiscussionTopics=(ListView) findViewById(R.id.lvDiscussionTopics);
-        arrayAdpt = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listOfDiscussion);
-        lvDiscussionTopics.setAdapter(arrayAdpt);
+        //set ListView adapter first
+        adapter = new MessageAdapter(this, R.layout.left_chat_bubble, ChatBubbles);
+        listView.setAdapter(adapter);
 
-
-        getUserName();
-
-        dbr.addValueEventListener(new ValueEventListener() {
+        //event for button SEND
+        btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot){
-                Set<String> set = new HashSet<String>();
-                Iterator i = dataSnapshot.getChildren().iterator();
-
-                while(i.hasNext()) {
-                    set.add(((DataSnapshot)i.next()).getKey());
+            public void onClick(View v) {
+                if (editText.getText().toString().trim().equals("")) {
+                    Toast.makeText(MainActivity.this, "Please input some text...", Toast.LENGTH_SHORT).show();
+                } else {
+                    //add message to list
+                    ChatBubble ChatBubble = new ChatBubble(editText.getText().toString(), myMessage);
+                    ChatBubbles.add(ChatBubble);
+                    adapter.notifyDataSetChanged();
+                    editText.setText("");
+                    if (myMessage) {
+                        myMessage = false;
+                    } else {
+                        myMessage = true;
+                    }
                 }
-                arrayAdpt.clear();
-                arrayAdpt.addAll(set);
-                arrayAdpt.notifyDataSetChanged();
-            }
-            @Override
-            public void onCancelled(DatabaseError databaseError){
             }
         });
-
-
-
-
-
-        lvDiscussionTopics.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent i = new Intent(getApplicationContext(), DiscussionActivity.class);
-                i.putExtra("selected_topic", ((TextView)view).getText().toString());
-                i.putExtra("user_name", UserName);
-                startActivity(i);
-            }
-        });
-
-    }
-    private void getUserName(){
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        final EditText userName = new EditText(this);
-
-        builder.setView(userName);
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener(){
-            @Override
-            public void onClick(DialogInterface dialog, int which){
-                UserName = userName.getText().toString();
-            }
-        });
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
-                @Override
-                public void onClick(DialogInterface dialog, int which){
-                 getUserName();
-               }
-        });
-        builder.show();
     }
 }
---
+++ PinkPineapleDating2/app/src/main/java/com/pinkpineaple/pinkpineapledating/UserDetails.java
@@ -0,0 +1,7 @@
+package com.pinkpineaple.pinkpineapledating;
+
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
